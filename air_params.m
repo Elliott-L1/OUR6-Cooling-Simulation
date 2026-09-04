19c48b3fb2a9c294f85f286d2c6c42946a888ddb
Elliott-L1 <yile.li@merton.ox.ac.uk>
2026-09-04 11:15:21 +0100

    Add files via upload

---
+++ air_params.m
@@ -0,0 +1,12 @@
+area_air    = 0.0353;   % A_fan [m^2]
+rho_air     = 1.225;    % air density [kg/m^3]
+cp_air      = 1005;     % air specific heat capacity [J/(kg*K)]
+
+vel_air_nom = 0.18878;  % nominal air velocity [m/s]
+% OUR5: 400 CMF
+
+HC_nom      = 266.43;   % nominal heat transfer coefficient
+%Estimated. Radiator data sheet only gave nominal heat transfer rate.
+%Nominal temperature difference is estimated to be 55 degree C.
+
+HC_min      = 1;      % minimum heat transfer coefficient
